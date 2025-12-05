62735f0dfcdc7616588b3c6a19a84a508e24a8d7
steffe <stefan.morin@live.se>
2025-12-05 02:19:57 +0100

    new config :D

--- init.lua
+++ init.lua
@@ -2,9 +2,9 @@ vim.o.number = true
 vim.o.signcolumn = "yes"
 vim.o.relativenumber = true
 vim.o.wrap = false
-vim.o.tabstop = 4
 vim.o.shiftwidth = 2
 vim.o.swapfile = false
+vim.o.clipboard = "unnamedplus"
 vim.g.mapleader = " "
 
 vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
@@ -16,18 +16,18 @@ require "config.lazy"
 require "mini.statusline".setup({use_icons = true})
 require "mini.pick".setup()
 require "mini.git".setup()
+require "oil".setup()
 
 vim.cmd("colorscheme vague")
 
---require "oil".setup()
 vim.keymap.set('n', '<leader>f', ':Pick files<CR>')
 vim.keymap.set('n', '<leader>h', ':Pick help<CR>')
---vim.keymap.set('n', '<leader>e', ':Oil<CR>')
+vim.keymap.set('n', '<leader>e', ':Oil<CR>')
 vim.keymap.set('n', '<leader>gs', ':Git status<CR>')
 vim.keymap.set('n', '<leader>ga', ':Git add')
 vim.keymap.set('n', '<leader>ga.', ':Git add .<CR>')
-vim.keymap.set('n', '<leader>gc', ':Git commit <CR>')
-vim.keymap.set('n', '<leader>gp', ':Git push')
+vim.keymap.set('n', '<leader>gc', ':Git commit<CR>')
+vim.keymap.set('n', '<leader>gp', ':Git push<CR>')
 
 vim.lsp.enable({"lua_ls"})
 vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
